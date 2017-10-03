af2b13218eb5ba3d6c55d0dd4b5302a28cfad37d
corazzon <joeunpark@gmail.com>
2017-10-03 22:52:42 +0900

    add chapter03

---
+++ chapter03/prun1.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def func_a():
+    a = np.random.randn(500, 500)
+    return a**2
+
+
+def func_b():
+    a = np.random.randn(1000,1000)
+    return a**2
+
+def func_both():
+    a = func_a()
+    b = func_b()
+    return [a, b]
+
+if __name__=='__main__':
+    func_both()
